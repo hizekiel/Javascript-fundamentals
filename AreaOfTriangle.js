0c71bacc49cfa9b6f66761f31bef9ac6c5e456fe
hizekiel <hizekielr@gmail.com>
2022-06-01 14:11:20 +0300

    second commit

---
+++ AreaOfTriangle.js
@@ -0,0 +1,7 @@
+function Area(base, height) {
+    AreaofT = 0.5 * base * height;
+    return AreaofT;
+
+}
+AreaOfATriangle = Area(3, 4);
+console.log('The area of the triangle is' + " " + AreaOfATriangle);
---
+++ Gusses-sum.js
@@ -0,0 +1,7 @@
+let sum = 0;
+for (i = 1; i < 100; i++) {
+    sum = sum + i
+    console.log(i + '+');
+
+}
+console.log('the sum is ' + ' ' + sum);
