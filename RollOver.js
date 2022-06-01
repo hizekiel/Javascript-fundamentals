a7b3c72d74c0c6abdf3ea113f0985247f936d268
hizekiel <hizekielr@gmail.com>
2022-06-01 12:30:09 +0300

    basic js

---
+++ RollOver.js
@@ -0,0 +1,14 @@
+function rollOver() {
+    for (i = 10; i > 0; i--) {
+        for (j = 1; j < 10; j++) {
+
+            console.log(i + 'in the bed,and ' + j);
+        }
+
+
+    }
+
+    console.log('no one left');
+    return
+}
+rollOver();
---
+++ ShipingCost.js
@@ -0,0 +1,20 @@
+/*The program calculates the shiping cost based on the location*/
+function calshipingCost(cost, location) {
+    let shippingPrice;
+    if (location === 'EU') {
+        shippingPrice = 5;
+        newPrice = cost + shippingPrice;
+        return console.log('you have to pay a total of' + " " + newPrice + "€ " + cost + "€ " + 'for the product and ' + shippingPrice + "€ " + 'for shipping');
+    } else if (location === 'US' || location === 'Canada') {
+        shippingPrice = 15;
+        newPrice = cost + shippingPrice;
+        return console.log('you have to pay a total of' + " " + newPrice + "€ " + cost + "€ " + 'for the product and ' + shippingPrice + "€ " + 'for shipping');
+    } else {
+        shippingPrice = 20;
+        newPrice = cost + shippingPrice;
+        return console.log('you have to pay a total of' + " " + newPrice + "€ " + cost + "€ " + 'for the product and ' + shippingPrice + "€ " + 'for shipping');
+
+    }
+
+}
+calshipingCost(30, 'US');
